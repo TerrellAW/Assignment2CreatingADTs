5e3bdcb0871f053344a959067cf3ca7c3c4302bf
TerrellAW <terraw1999@proton.me>
2025-10-21 17:56:35 -0600

    Add: size(), clear() and contains() methods

--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -33,5 +33,30 @@ public interface StackADT<E>
 	 * @return The item at the top of the stack.
 	 */
 	public E peek();
+
+	/**
+	 * Count the amount of items in the stack and return the number.
+	 *
+	 * @author TerrellAW
+	 * @return Integer containing total amount of items in the stack.
+	 */
+	public int size();
+
+	/**
+	 * Clears the entire stack.
+	 *
+	 * @author TerrellAW
+	 */
+	public void clear();
+
+	/**
+	 * Check if the stack contains the given value.
+	 * Return true if the value exists in the stack, false if it does not.
+	 *
+	 * @author TerrellAW
+	 * @param value is the search term the method will try to find.
+	 * @return <code>True</code> if the <code>value</code> exist in the stack, <code>False</code> if it does not.
+	 */
+	public Boolean contains(E value);
 }
 
