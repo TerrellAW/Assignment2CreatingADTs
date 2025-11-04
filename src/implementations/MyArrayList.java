2ef21cbb3b7dc926cce9bd7897a7461d17c3d019
TerrellAW <terraw1999@proton.me>
2025-11-03 20:38:46 -0700

    Add: Remove methods

--- src/implementations/MyArrayList.java
+++ src/implementations/MyArrayList.java
@@ -41,12 +41,16 @@ public class MyArrayList<E> implements Iterator<E>, ListADT<E>
 
 		Object[] firstSegment = new Object[index + 1]; // Create array to hold first segment with empty space
 		Object[] secondSegment = new Object[array.length - index]; // Create array to hold second segment
+
 		System.arraycopy(array, 0, firstSegment, 0, index); // Copy first segment to new array
 		System.arraycopy(array, index, secondSegment, 0, array.length - index); // Copy second segment to new array
+
 		firstSegment[index] = toAdd; // Add element to empty space at the end of first segment
 		array = new Object[firstSegment.length + secondSegment.length]; // Create new array large enough for both segments
+
 	    System.arraycopy(firstSegment, 0, array, 0, firstSegment.length); // Copy first segment into new array
 		System.arraycopy(secondSegment, 0, array, firstSegment.length, secondSegment.length); // Copy second segment into new array
+
 		return true; // Add successful
 	}
 	
@@ -64,6 +68,7 @@ public class MyArrayList<E> implements Iterator<E>, ListADT<E>
 	   	System.arraycopy(array, 0, newArray, 0, array.length); // Copy data from old array
 		newArray[newArray.length - 1] = toAdd; // Add element to the new array
 		array = newArray; // Replace old array with new array
+
 		return true; // Add successful
 	}
 	
@@ -75,12 +80,48 @@ public class MyArrayList<E> implements Iterator<E>, ListADT<E>
 
 	}
 	
+	/**
+	 *
+	 *
+	 * @author TerrellAW
+	 */
 	public E remove( int index ) throws IndexOutOfBoundsException {
+		if (index < 0 || index > size()) { // Throw error if index is out of bounds
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size());
+		}
+		
+		E element = (E)array[index]; // Store element that will be removed
 
+		Object[] firstSegment = new Object[index - 1]; // Create array to hold first segment
+		Object[] secondSegment = new Object[array.length - index]; // Create array to hold second segment
+		
+		System.arraycopy(array, 0, firstSegment, 0, firstSegment.length); // Copy segment from before index
+		System.arraycopy(array, index + 1, secondSegment, 0, secondSegment.length); // Copy segment from after index
+		
+		array = new Object[firstSegment.length + secondSegment.length]; // Create new array large enough for both segments
+	    System.arraycopy(firstSegment, 0, array, 0, firstSegment.length); // Copy first segment into new array
+		System.arraycopy(secondSegment, 0, array, firstSegment.length, secondSegment.length); // Copy second segment into new array
+
+		return element; // Return removed element
 	}
 	
+	/**
+	 *
+	 *
+	 * @author TerrellAW
+	 */
 	public E remove( E toRemove ) throws NullPointerException {
+		if (toRemove == null) { // Throw error if element is null
+			throw new NullPointerException("Element to remove must exist");
+		}
 
+		E element = (E)array[index]; // Store element that will be removed
+
+		Object[] newArray = new Object[array.length - 1]; // Create shorter array
+		System.arraycopy(array, 0, newArray, 0, newArray.length); // Copy all but last element to shorter array
+		array = newArray; // Replace old array with new array
+
+		return element; // Return removed element
 	}
 	
 	public E set( int index, E toChange ) throws NullPointerException, IndexOutOfBoundsException {
