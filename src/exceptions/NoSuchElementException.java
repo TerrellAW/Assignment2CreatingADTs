594c0bf6034787ffa084693fc9b50caf33e433fa
TerrellAW <terraw1999@proton.me>
2025-11-05 21:37:03 -0700

    Add: NoSuchElementException class

---
+++ src/exceptions/NoSuchElementException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class NoSuchElementException extends Exception {
+	public NoSuchElementException() {
+		super();
+	}
+
+	public NoSuchElementException( String message ) {
+		super( message );
+	}
+}
